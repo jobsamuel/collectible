ca0f360ad9d253fef7735227c0b00fe96693b735
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2021-10-21 08:06:51 -0400

    chore(nft): remove compile warning

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Unlicensed
 
-pragma solidity 0.8.4;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
